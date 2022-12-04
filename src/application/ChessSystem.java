3e367e4cbe93c8255321ec2aa4eeabd1bf9e1390
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2022-12-03 23:34:31 -0300

    first commit

---
+++ src/application/ChessSystem.java
@@ -0,0 +1,15 @@
+package application;
+
+/**
+ *
+ * @author danie
+ */
+public class ChessSystem {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        
+    }
+
+}
