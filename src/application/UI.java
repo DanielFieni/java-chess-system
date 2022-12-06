848023fdeb332b5a0de34ac6a7439ec68290dd24
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2022-12-06 09:20:13 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,7 +31,7 @@ public class UI {
         {
             System.out.print("-");
         } else {
-            System.out.println(piece);
+            System.out.print(piece);
         }
         System.out.print(" ");
     }
---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,23 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ *
+ * @author danie
+ */
+public class Rook extends ChessPiece{
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "R";
+    }
+
+}
