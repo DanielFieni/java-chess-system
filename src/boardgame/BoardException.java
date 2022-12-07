330bc6d8bbf85577a6caa386acc9b7cf8ae24cdf
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2022-12-06 21:43:03 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+/**
+ *
+ * @author danie
+ */
+public class BoardException extends RuntimeException{
+
+    public BoardException(String mensagem)
+    {
+        super(mensagem);
+    }
+    
+}
