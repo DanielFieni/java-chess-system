29c14f6d6b4ff63f68ec22d0e4b03ee88f503571
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2022-12-15 13:45:44 -0300

    Handling exceptions and clearing screen

--- src/application/ChessSystem.java
+++ src/application/ChessSystem.java
@@ -2,9 +2,11 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -17,22 +19,31 @@ public class ChessSystem {
 
         Scanner scan = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        
-        while(true)
-        {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(scan);
-            
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(scan);
-            
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            
-        }
-        
-    }
 
+        while (true) {
+            try
+            {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(scan);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(scan);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException e){
+            System.out.println(e.getMessage());
+            scan.nextLine();
+            
+            }
+            catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                scan.nextLine();
+            }
+        }
+    }
 }
